881bf66478efcd0c7eed71bc806affc4ec820736
meriam <hamdaoui.meriam85@gmail.com>
2022-04-04 15:05:19 +0200

    end of the day not much update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import './App.css';
+import Filter from './Components/filter/Filter';
 import MovieList from './Components/lists/MovieList';
 import Movies from './Movies'
 
 function App() {
   const [movies, setMovies] = useState(Movies)
   return (<div className='App'>
+    <Filter />
     {/* btn add movie redirect to Add.js */}
     <MovieList movies={movies}/>     
   </div>);}
